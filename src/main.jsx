55b0f0e507f77cbaad9711a06b7223385b26d53b
Testicode <osin7179@gmail.com>
2025-04-18 12:43:32 +0100

    Updated rest-api code
    Made a few changes to get the right file path

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,26 @@ const router = createBrowserRouter([
     path: "/details/:name", // Dynamic route for country details
     element: <Details />,
   },
+
+  {
+    path: "*",
+    element: (
+      <div className="flex justify-center items-center h-screen">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/5613/5613910.png"
+          alt="404"
+          className="w-1/4 h-1/4"
+        />
+        <h1 className="text-4xl font-bold font-[Nunito sans]">
+          404 Page not found
+        </h1>
+      </div>
+    ),
+  },
+
+  {
+    basename: "/RESTful-API-Countries",
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
