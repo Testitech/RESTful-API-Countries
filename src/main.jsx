29112fed982e08354215ee48b4d2d21e63499802
Testicode <osin7179@gmail.com>
2025-04-18 13:59:17 +0100

    Added Error Page

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,10 @@ const router = createBrowserRouter([
     path: "/details/:name", // Dynamic route for country details
     element: <Details />,
   },
+  {
+    path: "*",
+    element: <div>404 Not Found</div>,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
