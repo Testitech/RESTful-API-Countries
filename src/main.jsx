072710c1f5a6980ec193fe5d54ed5c7d961cc0b8
Testicode <osin7179@gmail.com>
2025-04-22 11:36:35 +0100

    Updated main.jsx ⚡
    Fixed the slightest mistake which gave me headache

--- src/main.jsx
+++ src/main.jsx
@@ -5,36 +5,29 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App"; // Ensure App is imported
 import Details from "./[name]/Details"; // Dynamic route component
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/details/:name", // Dynamic route for country details
-    element: <Details />,
-  },
-
-  {
-    path: "*",
-    element: (
-      <div className="flex justify-center items-center h-screen">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/5613/5613910.png"
-          alt="404"
-          className="w-1/4 h-1/4"
-        />
-        <h1 className="text-4xl font-bold font-[Nunito sans]">
-          404 Page not found
-        </h1>
-      </div>
-    ),
-  },
-
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/details/:name",
+      element: <Details />,
+    },
+    {
+      path: "*",
+      element: (
+        <div className="flex items-center justify-center h-screen font-[Nunito sans] font-bold">
+          <h1 className="text-4xl">404 Not Found</h1>
+        </div>
+      ),
+    },
+  ],
   {
     basename: "/RESTful-API-Countries",
-  },
-]);
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
